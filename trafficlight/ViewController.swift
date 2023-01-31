0c7b61cda830a8fa983b836cca0a3fa843a3d712
Ilias <cepctapk@gmail.com>
2023-01-31 16:34:30 +0300

    added action for work the trafficlight

--- trafficlight/ViewController.swift
+++ trafficlight/ViewController.swift
@@ -14,16 +14,41 @@ class ViewController: UIViewController {
     @IBOutlet var viewGreen: UIView!
     @IBOutlet var toggleButton: UIButton!
     
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         viewRed.layer.cornerRadius = 50.0
         setColorRed(0.3)
+        
         viewYellow.layer.cornerRadius = 50.0
         setColorYellow(0.3)
+        
         viewGreen.layer.cornerRadius = 50.0
         setColorGreen(0.3)
+        
         toggleButton.layer.cornerRadius = 10
-        toggleButton.setTitle("START", for: .normal)
+        toggleButton.setTitle("START", for: .normal)
+      
+    }
+    
+    @IBAction func toggleOfLight () {
+        toggleButton.setTitle("NEXT", for: .normal)
+        setColorRed(0.3)
+        setColorYellow(0.3)
+        setColorGreen(0.3)
+        
+        if count == 1 {
+            setColorRed(1)
+            count = 2
+        } else if count == 2 {
+            setColorYellow(1)
+            count = 3
+        } else  {
+            setColorGreen(1)
+            count = 1
+        }
     }
     
     func setColorRed(_ alpha: CGFloat) {
@@ -37,6 +62,8 @@ class ViewController: UIViewController {
     func setColorGreen(_ alpha: CGFloat) {
         viewGreen.backgroundColor = UIColor.green.withAlphaComponent(alpha)
     }
+    
+    
     
 
 
