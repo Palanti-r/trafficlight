067d7327cefa174376344a3759f0ead02a8fc26e
Ilias <cepctapk@gmail.com>
2023-01-31 14:47:40 +0300

    added referencing

--- trafficlight/ViewController.swift
+++ trafficlight/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewRed: UIView!
+    @IBOutlet var viewYellow: UIView!
+    @IBOutlet var viewGreen: UIView!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
